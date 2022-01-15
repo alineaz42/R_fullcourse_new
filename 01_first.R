72b204782c3d05b336af992d232698207ba9f0d5
Ali Neaz <neazali77@gmail.com>
2022-01-16 02:12:20 +0600

    started the course again the second video was done

---
+++ 01_first.R
@@ -0,0 +1,37 @@
+x <- 41
+print(x)
+x
+
+y <- 7
+
+y
+
+
+x <- 9
+sqrt(x)
+
+# ls(var,var) to know whats in the memory
+ls()
+
+# remove 
+rm(y)
+# y is removed
+ls()
+
+# object name 
+x.1 <- 47
+x.1
+# object namings
+xx <- "marin"
+
+sqrt(x)
+log(x)
+exp(x)
+floor(x)
+round(x)
+log10(x)
+log2(x)
+abs(-x)
+
+
+# comments use hash sign or ctrl+shif+c in rstudio to comment or uncomment
