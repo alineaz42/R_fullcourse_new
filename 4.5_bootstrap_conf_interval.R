6024a61d4f05fec384d9c03c1a1bdfa5365bc793
Ali Neaz <neazali77@gmail.com>
2022-02-06 02:36:19 +0600

    done bootstrap confidence interval making need more practice

--- 4.5_bootstrap_conf_interval.R
+++ 4.5_bootstrap_conf_interval.R
@@ -43,3 +43,36 @@ n.c <- 12
 n.m <- 11
 B <- 10000
 
+Boot.casein <- matrix(sample(weight[feed=="casein"],size=B*n.c,replace=T),ncol=B,nrow=n.c)
+Boot.meatmeal <- matrix(sample(weight[feed=="meatmeal"],size=B*n.m,replace=T),ncol=B,nrow=n.m)
+
+dim(Boot.casein);dim(Boot.meatmeal)
+Boot.casein[1:5,1:5]
+
+#--------------------
+Boot.Diff.In.Means <- colMeans(Boot.casein)-colMeans(Boot.meatmeal)
+length(Boot.Diff.In.Means)
+Boot.Diff.In.Means[1:20]
+
+
+Boot.Diff.In.Medians <- apply(Boot.casein,2,median)-apply(Boot.meatmeal,2,median)
+Boot.Diff.In.Medians[1:5]
+length(Boot.Diff.In.Medians)
+
+
+# Making the confidence intervals with 95%
+#1. Percentile Method 2. Basic Method
+#3. Normal Method 4. Bias-Corrected method
+
+
+# percentila method for the means
+quantile(Boot.Diff.In.Means,probs = 0.025)
+quantile(Boot.Diff.In.Means,probs = 0.975)
+
+
+# percentile method for the medians
+quantile(Boot.Diff.In.Medians,probs=0.025)
+quantile(Boot.Diff.In.Medians,probs=0.975)
+
+
+
